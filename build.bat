ba97d7d1032fc59daa94d0ac8816c0ec165646c2
Andrew Bird <ajb@spheresystems.co.uk>
2021-02-16 15:04:56 +0000

    Build: Import usual build scripts

---
+++ build.bat
@@ -0,0 +1,42 @@
+goto %1
+
+:watcom
+rem ############# WATCOM ########################
+set PATH=C:\bin;C:\watcom\binw;%PATH%
+rem # -we treat all warnings as errors
+rem # -wx set warning level to max
+rem # -zq operate quietly
+rem # -fm[=map_file]  generate map file
+rem # -fe=executable  name executable file
+rem # -m{t,s,m,c,l,h}  memory model
+
+set WATCOM=C:\watcom
+set INCLUDE=C:\watcom\h
+set CC=wcl
+set EXEFLAGS=-mt
+set CFLAGS=-oas -bt=DOS -bcl=DOS -D__MSDOS__ -zp1 -s -0 -wx -we -zq -fm %EXEFLAGS% -fe=
+set TARGET=label.exe
+goto doit
+
+:tcc
+rem ############# TURBO_C ########################
+set PATH=C:\bin;C:\tc\bin;%PATH%
+rem # -w warn -M create map -f- no floating point -Z register optimize
+rem # -O jump optimize -k- no standard stack frome -K unsigned char
+rem # -exxx executable name (must be last) -mt tiny (default is small)
+rem # -N stack checking -a- byte alignment  -ln no default libs
+rem # -lt create .com file -lx no map file ...
+rem # tiny has near qsort / malloc pointers, very limited!
+rem # compact large: large has far function pointers, compact only far heap.
+rem # makes no real difference here, but compact saves a few bytes in size.
+
+set CC=tcc
+set EXEFLAGS=-mt -Z -O -k-
+set CFLAGS=-w -M -f- -a- -K -ln %EXEFLAGS% -e
+set TARGET=label.exe
+goto doit
+
+:doit
+
+rem We use GNU make for all targets
+make -C src %TARGET%
