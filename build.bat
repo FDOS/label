927c801568f4e8526e3b18fb4f782caa175f8834
Kenneth J Davis <PerditionC@gmail.com>
2021-11-18 22:23:01 -0500

    fix building under DOS and Windows with build.bat using Watcom and Borland tcc or bcc compilers

--- build.bat
+++ build.bat
@@ -1,4 +1,17 @@
+@echo off
+
+if "%1"=="NOCATS" set NOCATS=-DNOCATS
+if "%1"=="NOCATS" shift
+if "%1"=="" goto usage
 goto %1
+
+:usage
+echo Please run build compiler
+echo where compiler is one of watcom or tcc
+echo e.g.  build watcom
+echo Optional pass NOCATS to build without cats
+echo e.g.  build NOCATS tcc
+goto done
 
 :watcom
 rem ############# WATCOM ########################
@@ -10,9 +23,9 @@ rem # -fm[=map_file]  generate map file
 rem # -fe=executable  name executable file
 rem # -m{t,s,m,c,l,h}  memory model
 
-set WATCOM=C:\watcom
-set INCLUDE=C:\watcom\h
-set CC=wcl
+if "%WATCOM%"=="" set WATCOM=C:\watcom
+if "%INCLUDE%"==""set INCLUDE=%WATCOM%\h
+set COMPILER=watcom
 set EXEFLAGS=-mt
 set CFLAGS=-oas -bt=DOS -bcl=DOS -D__MSDOS__ -zp1 -s -0 -wx -we -zq -fm %EXEFLAGS% -fe=
 set TARGET=label.exe
@@ -30,7 +43,9 @@ rem # tiny has near qsort / malloc pointers, very limited!
 rem # compact large: large has far function pointers, compact only far heap.
 rem # makes no real difference here, but compact saves a few bytes in size.
 
-set CC=tcc
+set COMPILER=borland
+rem change to 0 if using Borland C/C++ instead of Turbo C/C++
+if "%TURBO%"=="" set TURBO=1
 set EXEFLAGS=-mt -Z -O -k-
 set CFLAGS=-w -M -f- -a- -K -ln %EXEFLAGS% -e
 rem tcc looks for includes from the current directory, not the location of the
@@ -40,9 +55,18 @@ set TARGET=label.exe
 goto doit
 
 :doit
-rem # if you want to build without kitten uncomment the following
-rem set CFLAGS=-DNOCATS %CFLAGS%
-rem set TARGET=_%TARGET%
+rem # if you want to build without kitten call with NOCATS before compiler
+set CFLAGS=%NOCATS% %CFLAGS%
+if not "%NOCATS%"=="" set TARGET=_%TARGET%
 
 rem We use GNU make for all targets
 make -C src %TARGET%
+
+:done
+rem we clear all these env variables (even if set prior to calling)
+rem do do not clear TURBO, WATCOM, or INCLUDE if set
+set TARGET=
+set COMPILER=
+set CFLAGS=
+set EXEFLAGS=
+set NOCATS=
