0fb5c67e33a7041226c7e5c20db7eb00dbb9b155
Andrew Bird <ajb@spheresystems.co.uk>
2021-11-20 09:06:46 +0000

    Initialise ES register on Truename call

--- src/label.c
+++ src/label.c
@@ -279,8 +279,10 @@ int valid_drive(char *s)
     strcpy(buf1, TempDrive);
     strcat(buf1, "\\");
     segread(&sregs);
-    regs.x.si = (unsigned)buf1; /* needs NEAR POINTER */
-    regs.x.di = (unsigned)buf2; /* needs NEAR POINTER */
+    sregs.ds = FP_SEG(buf1);
+    regs.x.si = FP_OFF(buf1);
+    sregs.es = FP_SEG(buf2);
+    regs.x.di = FP_OFF(buf2);
     regs.x.ax = 0x6000;
     intdosx(&regs, &regs, &sregs);
     if (*buf1 != *buf2)
