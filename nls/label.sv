a0f77803b1105bfaecdf91de7e11f1a4de7a2113
Andrew Bird <ajb@spheresystems.co.uk>
2022-02-06 10:37:00 +0000

    Translations: Update from fd-nls

--- nls/label.sv
+++ nls/label.sv
@@ -7,7 +7,7 @@
 0.3:  [enhet:]  Anger vilken enhet du vill sätta etikett på\n
 0.4:  [etikett] Anger den nya etiketten du vill sätta på enheten\n
 0.5:  /?        Visar detta hjälpmeddelande\n
-1.0:Ta bort aktuell volymetikett (Y/N)? 
+1.0:Ta bort aktuell volymetikett (J/N)? 
 1.1:Volymetikett (11 tecken, ENTER för ingen)? 
 1.2:Volym i enhet %c saknar etikett\n
 1.3:Volym i enhet %c är %s\n
@@ -19,5 +19,5 @@
 2.4:Ogiltig etikett\n
 2.5:Inte en giltig enhet\n
 2.6:Du kan inte sätta etikett på en nätverksenhet\n
-2.7:Du kan inte sätta etikett på en enhet som har\nanvänder ASSIGN, JOIN eller SUBST.\n
+2.7:Du kan inte sätta etikett på en enhet som\nanvänder ASSIGN, JOIN eller SUBST.\n
 2.8:Ogiltig enhet\n
